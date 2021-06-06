66ca42f88d2d46f49b2e4cf56a8982e8f19fc1de
Alvatrox4 <alvaro.rbarrantes@gmail.com>
2021-06-05 20:32:27 -0600

    Creacion del Modelo

---
+++ Infraestructure/Models/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Models
+{
+    public class MyContext:SunriseBDEntities
+    {
+        public MyContext() { }
+            
+    }
+}
