c05a7f32a91d79de12adfad3cf3a76c8b08fcc78
John Paul Alfaro <Johnpaul899@hotmail.com>
2021-07-10 17:43:35 -0600

    Archivos de Movimientos

---
+++ Web/Controllers/MovimientoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class MovimientoController : Controller
+    {
+
+        // GET: Movimiento
+        public ActionResult MovimientoEntrada()
+        {
+            return View();
+        }
+
+        public ActionResult MovimientoSalida()
+        {
+            return View();
+        }
+
+        public ActionResult MovimientoTransferencia()
+        {
+            return View();
+        }
+
+
+
+    }
+}
