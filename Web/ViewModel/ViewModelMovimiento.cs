651bda488da5fa9fd9272206028a5c7b0545e0a5
Alvatrox4 <alvaro.rbarrantes@gmail.com>
2021-07-10 18:18:39 -0600

    ViewModel Movimiento

---
+++ Web/ViewModel/ViewModelMovimiento.cs
@@ -0,0 +1,30 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModel
+{
+    public class ViewModelMovimiento
+    {
+        public MOVIMIENTO mov { get; set; }
+
+        public List<PRODUCTOS> prodList { get; set; }
+
+        public List<PRODUCTOS> prodListDetalle=new List<PRODUCTOS>();
+
+        public USUARIO usuario { get; set; }
+
+        public List<SUCURSAL> sucursalEntrada { get; set; }
+        public List<SUCURSAL> sucursalSalir { get; set; }
+
+        public HISTORICO historico { get; set; }
+
+        public List<HistDetalleEntradaSalida> historicoDetalle { get; set; }
+
+        public ViewModelMovimiento()
+        {
+        }
+    }
+}
